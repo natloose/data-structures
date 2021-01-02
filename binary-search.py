ee387f9d5a2e3a3875efbce148d72626ce1e9874
conorwilliams <conorpatrickwilliams@gmail.com>
2021-01-02 16:16:17 +0000

    binary search recursive

--- binary-search.py
+++ binary-search.py
@@ -7,25 +7,23 @@
 # The iterative approach is very simple and similar to the recursive approach. Here, we just perform the checks
 # in a while loop:
 
-arr = [1, 2, 3, 4, 7, 10, 15, 20, 28, 29, 35, 55, 89, 109]
-element = 3
+# arr = [1, 2, 3, 4, 7, 10, 15, 20, 28, 29, 35, 55, 89, 109]
+# element = 3
 
 
-def binary_search_recursive(arr, element):
-
+def binary_search_iterative(arr, element):
     start = 0
     mid = 0
     end = len(arr)
     step = 0
 
     while start <= end:
-
         print("Subarray in step {}: {}".format(step, str(arr[start:end + 1])))
         step = step + 1
         mid = (start + end) // 2
 
         if element == arr[mid]:
-            print(f"Number 3 is located at index {mid}.")
+            print(f"Element found at index {mid}.")
             break
         elif element < arr[mid]:
             end = mid - 1
@@ -33,4 +31,31 @@ def binary_search_recursive(arr, element):
             start = mid + 1
 
 
-binary_search_recursive(arr, element)
+# binary_search_iterative(arr, element)
+
+# Recursive
+
+
+start = 0
+arr = [1, 2, 5, 9, 12, 16, 25]
+end = len(arr)
+target = 16
+
+
+def binary_search_recursive(arr, target, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end) // 2
+
+    if target == arr[mid]:
+        print(mid)
+
+    if target < arr[mid]:
+        return binary_search_recursive(arr, target, start, mid-1)
+    else:
+        return binary_search_recursive(arr, target, mid+1, end)
+
+
+binary_search_recursive(arr, target, start, end)
+
